5dec701589e2b0caa8b9d326e0796679bd282a8c
Igor Artamonov <igor@artamonov.ru>
2017-08-31 22:47:38 -0400

    status checks for a fork

--- bin/status.py
+++ bin/status.py
@@ -8,6 +8,42 @@ import rpc_api
 
 all_nodes = ["g1", "g2", "gm1"]
 
+def same_fork(hashes):
+    return len(set(hashes)) == 1
+
+def find_fork(api, pods, no_fork_at, fork_at):
+    check_height = no_fork_at + int((fork_at - no_fork_at) / 2)
+    hashes = [api.get_hash(pod_id, hex(check_height))["hash"] for pod_id in pods]
+    if not same_fork(hashes):
+        return find_fork(api, pods, no_fork_at, check_height)
+    else:
+        gap = check_height - no_fork_at
+        if gap > 1:
+            return find_fork(api, pods, check_height, fork_at)
+        else:
+            return check_height + 1
+
+def check_fork():
+    print("Checking for a fork...")
+    api = rpc_api.RpcApi()
+    pods = api.get_nodes()
+    min_height = min([int(api.get_height(pod_id), 16) for pod_id in pods]) - 10
+    if min_height <= 0:
+        print("Ok. Blockchain has less than 10 blocks")
+        return 
+    hashes = [api.get_hash(pod_id, hex(min_height))["hash"] for pod_id in pods]
+    if not same_fork(hashes):
+        print("!!!! Fork detected !!!!")
+        fork_block = find_fork(api, pods, 0, min_height)
+        print("Fork happened at %s" % fork_block)
+        print("%s\t\t\t%s\t%s" % ("ID", "HEIGHT", "HASH"))
+        for pod_id in pods:
+            hash = api.get_hash(pod_id, fork_block)["hash"]
+            print("%s\t%s\t%s" % (pod_id, fork_block, hash))
+    else:
+        print("Ok. No Fork detected")
+
+
 def list():
     print("Nodes status")
     print("%s\t\t\t%s\t%s\t%s" % ("ID", "PEERS", "HEIGHT", "HASH"))
@@ -21,3 +57,4 @@ def list():
 
 if __name__ == "__main__":
     list()
+    check_fork()
