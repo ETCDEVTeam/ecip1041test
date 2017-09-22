19ec680f3591ce6bdfd95f087db4902829980b7a
Igor Artamonov <igor@artamonov.ru>
2017-09-22 00:23:59 -0400

    topology

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,5 +11,7 @@ gcloud config set compute/zone $PROJECT_ZONE
 gcloud config set container/cluster $PROJECT_CLUSTER
 gcloud container clusters get-credentials $PROJECT_CLUSTER
 
-kubectl create -f ./k8s/nodes.rc.yaml
 kubectl create -f ./k8s/nodes.svc.yaml
+kubectl create -f ./k8s/nodes.rc.yaml
+
+echo Please give 2-3 minutes to spin up nodes before calling bin/status.py
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-kubectl delete rc node-g1-rc node-g2-rc node-g3-rc node-gm1-rc node-gm2-rc node-p1-rc node-p2-rc node-p3-rc node-pm1-rc
-kubectl delete svc g1 g2 g3 gm1 gm2 p1 p2 p3 pm1
+kubectl delete rc node-gm1-rc node-gm2-rc
+kubectl delete rc node-g1-rc node-g2-rc node-g3-rc
+kubectl delete rc node-p1-rc node-p2-rc node-p3-rc
+kubectl delete svc gm1 gm2 g1 g2 g3 g4 p1 p2 p3
 
 echo "Stopped. Wait for pods termination..."
 
